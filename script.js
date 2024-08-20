dfce23dbc98c5e0684025bf9a552c7510604eba8
jaimezzapata <zapataval2304@gmail.com>
2024-08-20 08:44:01 -0500

    Se finaliza condicional multiple

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-let edad = 18;
-if (edad > 18) {
-  console.log("Es mayor de edad");
-} else if (edad == 18) {
-  console.log("Apenas cumplio la mayoría de edad");
-} else if(false) {
-  console.log("No hay datos");
-} else if(false) {
-    console.log('No se cumple')
-} else {
-    console.log('Ultima ejecución')
-}
+// let edad = 18;
+// let salario = 10000;
+// if (edad > 18 || salario >= 10000) {
+//   console.log("Es mayor de edad");
+// }
+// else if (edad == 18) {
+//   console.log("Apenas cumplio la mayoría de edad");
+// } else if(false) {
+//   console.log("No hay datos");
+// } else if(false) {
+//     console.log('No se cumple')
+// } else {
+//     console.log('Ultima ejecución')
+// }
+// let categoria = prompt("Ingrese la catengoria: 1- Tecnologia");
+
+// switch (categoria.toLowerCase()) {
+//   case "tecnologia":
+//     console.log("Esta es la categoria tecnologia");
+//     break;
+//   case "hogar":
+//     console.log("Este es el caso 1");
+//     break;
+//   case "servicios":
+//     console.log("Este es el otro caso");
+//     break;
+//   default:
+//     console.log("No no se cumple la condición");
+//     break;
+// }
