37ec1d450bfb4a0c90e15c7d4087246011496156
jaimezzapata <zapataval2304@gmail.com>
2024-08-20 08:13:09 -0500

    Se agrega sintaxis de condicionales simples, compuestos y anidados

---
+++ script.js
@@ -0,0 +1,12 @@
+let edad = 18;
+if (edad > 18) {
+  console.log("Es mayor de edad");
+} else if (edad == 18) {
+  console.log("Apenas cumplio la mayoría de edad");
+} else if(false) {
+  console.log("No hay datos");
+} else if(false) {
+    console.log('No se cumple')
+} else {
+    console.log('Ultima ejecución')
+}
